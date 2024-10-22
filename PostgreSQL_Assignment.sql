df818924bd8e9bab397abef5c8ceb999a5d4123a
Hosain <cryptichasan925@gmail.com>
2024-10-23 00:40:03 +0600

    parfectly mentioned everything

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -64,13 +64,13 @@ SELECT * FROM enrollment;
 
 
 
--- Query 1
+-- Query 1 : Insert a new student record
 INSERT INTO students (student_name, age, email, frontend_mark, backend_mark, status)
 VALUES
     ('Abu Hosain', 20, 'abuhosainmin@gmail.com', 60, 58, NULL);
 
 
--- Query 2
+-- Query 2 : Retrieve the names of all students who are enrolled in the course titled 'Next.js'
 
 SELECT s.student_name
 FROM students s
@@ -80,7 +80,9 @@ WHERE c.course_name = 'Next.js';
  
 
 
---  Query 3
+--  Query 3 : Update the status of the student with the highest total (frontend_mark + backend_mark) to 'Awarded'
+
+
 UPDATE students
 SET status = 'Awarded'
 WHERE student_id = (
@@ -91,7 +93,7 @@ WHERE student_id = (
 );
 
 
--- Query 4
+-- Query 4 : Delete all courses that have no students enrolled
 DELETE FROM courses
 WHERE course_id NOT IN (
     SELECT DISTINCT course_id
@@ -99,26 +101,26 @@ WHERE course_id NOT IN (
 );
 
 
--- Query 5 
+-- Query 5 : Retrieve the names of students using a limit of 2, starting from the 3rd student
 SELECT student_name
 FROM students
 ORDER BY student_id
 OFFSET 2 LIMIT 2;
 
 
--- Query 6
+-- Query 6 : Retrieve the course names and the number of students enrolled in each course
 SELECT c.course_name, COUNT(e.student_id) AS students_enrolled
 FROM courses c
 LEFT JOIN enrollment e ON c.course_id = e.course_id
 GROUP BY c.course_name;
 
 
--- Query 7
+-- Query 7 : Calculate and display the average age of all students
 SELECT ROUND(AVG(age), 2) AS average_age
 FROM students;
 
 
--- Query 8 
+-- Query 8 : Retrieve the names of students whose email addresses contain 'example.com'
 SELECT student_name
 FROM students
 WHERE email LIKE '%example.com%';
